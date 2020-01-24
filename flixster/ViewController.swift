7fb60649039d7546d048c0e80249e44a596b3b16
Sudiksha Sarvepalli <ssarvepa@ucsd.edu>
2020-01-23 17:58:48 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Sudiksha Sarvepalli on 1/23/20.
+//  Copyright © 2020 Sudiksha Sarvepalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
