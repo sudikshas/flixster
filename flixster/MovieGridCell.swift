aa0491128921e829e9754159c7b818c58599774e
Sudiksha Sarvepalli <ssarvepa@ucsd.edu>
2020-02-05 23:16:06 -0800

    flix part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Sudiksha Sarvepalli on 2/5/20.
+//  Copyright © 2020 Sudiksha Sarvepalli. All rights reserved.
+//
+
+import UIKit
+
+
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
